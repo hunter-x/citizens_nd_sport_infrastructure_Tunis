9d28c769fe0051abb0b4fe6584adbaf070aa3c66
hunter-x <aa.test20@gmail.com>
2018-04-26 18:30:19 +0100

    adding react router

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home' ;
+export default class App extends Component {
+  render() {
+    return (
+      <Switch>
+      <Route exact path="/" component={Home} />
+      </Switch>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 //import ReduxPromise from "redux-promise";
 import { AppContainer } from 'react-hot-loader'
+import { Route, Router, Redirect, Switch,BrowserRouter } from 'react-router-dom';
 
-import App from './components/App';
+import App from './App';
 import reducers from './reducers';
+import history from './history';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 //const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
@@ -15,7 +17,9 @@ const render = Component => {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <AppContainer>
+      <Router history={history}>
         <App />
+        </Router>
       </AppContainer>
     </Provider>,
     document.getElementById('container')
@@ -25,7 +29,7 @@ const render = Component => {
 render(App)
 
 if (module.hot) {
-  module.hot.accept('./components/App', () => { render(App) })
+  module.hot.accept('./App', () => { render(App) })
 }
 
 
