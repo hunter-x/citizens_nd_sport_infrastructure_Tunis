5781b7ac2285f557b63a8ddb4e3824df2afd0e03
hunter-x <aa.test20@gmail.com>
2018-04-26 18:40:24 +0100

    adding language support

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-//import ReduxPromise from "redux-promise";
 import { AppContainer } from 'react-hot-loader'
 import { Route, Router, Redirect, Switch,BrowserRouter } from 'react-router-dom';
-
 import App from './App';
 import reducers from './reducers';
 import history from './history';
-
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-//const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+import counterpart  from 'counterpart';
+import Translate    from 'react-translate-component';
 
+counterpart.registerTranslations('en',require('../locales/en'));
+counterpart.registerTranslations('fr',require('../locales/fr'));
+counterpart.registerTranslations('ar',require('../locales/ar'));
 const render = Component => {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
